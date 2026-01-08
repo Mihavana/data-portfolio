fc25e4949aea1df533d67a1718ba375ed82c7c32
Mihavana RAHOLDINA <mihavana.raholdina@gmail.com>
2026-01-08 21:03:33 +0300

    Enhance transform function to include date conversion, numeric coercion, and column renaming

--- data-engineering/stock-etl-pipeline/src/transform.py
+++ data-engineering/stock-etl-pipeline/src/transform.py
@@ -2,3 +2,37 @@ import pandas as pd
 
 def transform(df : pd.DataFrame) -> pd.DataFrame:
     df.columns = df.columns.str.strip()
+
+    df["Date"] = pd.to_datetime(df["Date"])
+
+    numeric_cols = [
+        "Open", "High", "Low", "Close",
+        "Volume", "Dividends", "Stock Splits"
+    ]
+
+    for col in numeric_cols:
+        df[col] = pd.to_numeric(df[col], errors="coerce")
+
+    df = df.rename(columns={
+        "Date": "date",
+        "Open": "open",
+        "High": "high",
+        "Low": "low",
+        "Close": "close",
+        "Volume": "volume",
+        "Dividends": "dividends",
+        "Stock Splits": "stock_splits"
+    })
+
+    df = df[
+        [
+            "symbol", "date", "open", "high",
+            "low", "close", "volume",
+            "dividends", "stock_splits",
+            "extracted_at"
+        ]
+    ]
+
+    df = df.drop_duplicates(subset=["symbol", "date"])
+
+    return df
